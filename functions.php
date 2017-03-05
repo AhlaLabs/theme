96cfc233c74075c5026d8b33f124d0b67feed9b9
hamad-almamari <alonedream15h@gmail.com>
2017-03-05 21:42:02 +0400

    Move _s files to there folder

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@
  */
 
 require_once get_template_directory() . '/inc/core/autoload.php';
+require_once get_template_directory() . '/inc/_s/autoload.php';
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -20,29 +21,3 @@ function ahla_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'ahla_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'ahla_content_width', 0 );
-
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
